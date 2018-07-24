172084e2f469b093b023fa81c4f0ba8d00b8fd1c
Lisa Palathingal <lipalath@microsoft.com>
2018-07-24 12:20:59 -0700

    Set up Nutrition App

---
+++ WeatherApp/www/scripts/weather.js
@@ -0,0 +1,44 @@
+﻿var OpenWeatherAppKey = "ea29d5ccb25f6ed17f3ae584ff006a7c";
+
+function getWeatherWithZipCode() {
+    var zipcode = $('#zip-code-input').val();
+    var queryString =
+        'http://api.openweathermap.org/data/2.5/weather?zip='
+        + zipcode + ',us&appid=' + OpenWeatherAppKey + '&units=imperial';
+    $.getJSON(queryString, function (results) {
+        showWeatherData(results);
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+    });
+    return false;
+}
+
+function displayRegistrationForm() {
+    return true;
+}
+
+function showWeatherData(results) {
+
+    if (results.weather.length) {
+        $('#error-msg').hide();
+        $('#weather-data').show();
+
+        $('#title').text(results.name);
+        $('#temperature').text(results.main.temp);
+        $('#wind').text(results.wind.speed);
+        $('#humidity').text(results.main.humidity);
+        $('#visibility').text(results.weather[0].main);
+
+        var sunriseDate = new Date(results.sys.sunrise * 1000);
+        $('#sunrise').text(sunriseDate.toLocaleTimeString());
+
+        var sunsetDate = new Date(results.sys.sunset * 1000);
+        $('#sunset').text(sunsetDate.toLocaleTimeString());
+
+    } else {
+        $('#weather-data').hide();
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    }
+}
